6d9a55e65cfae9aab51299e32374f9a6bc5571fb
Matthew <theyeas@gmail.com>
2010-11-10 15:15:33 -0600

    Complete, for now

--- BPio.java
+++ BPio.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 public interface BPio {
-	public abstract boolean allowPlayer(BPArea area, String playerName, permissionLevel level);
-	public abstract boolean allowGroup(BPArea area, String groupName, permissionLevel level);
-	public abstract boolean playerAllowed(BPArea area, String playerName, permissionLevel level);
+	public abstract boolean allowPlayer(BPArea area, String playerName, BPArea.permissionLevel level);
+	public abstract boolean allowGroup(BPArea area, String groupName, BPArea.permissionLevel level);
+	public abstract boolean playerAllowed(BPArea area, String playerName, BPArea.permissionLevel level);
 	//checks to see if a player is at least permissionLevel level. 
-	public abstract ArrayList<String> groupsAllowed(BPArea area, permissionLevel minLevel);
+	public abstract ArrayList<String> groupsAllowed(BPArea area, BPArea.permissionLevel minLevel);
 	public abstract boolean protect(BPArea area);
 	public abstract boolean unprotect(BPArea area);
 	public abstract boolean delete(BPArea area);
@@ -13,6 +13,7 @@ public interface BPio {
 	public abstract BPArea get(int id);
 	public abstract BPArea getLeading(int x, int y, int z);
 	public abstract ArrayList<BPArea> getAll(int x, int y, int z);
-	public abstract ArrayList<BPArea> getAll(String playerName, permissionLevel minLevel);
-	public abstract boolean hasUnownedInArea(BPArea area);
+	public abstract ArrayList<BPArea> getAll(String playerName, BPArea.permissionLevel minLevel);
+	public abstract boolean isAdminInAllAreas(BPArea area);
+	public abstract boolean validGroupname(String groupName);
 }
